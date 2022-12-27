f979e98d6eab003d6106a35a5549279c10790cac
Artem Martynov <amerta90@mail.ru>
2022-12-27 20:45:21 +0300

    Task 1 was created

---
+++ Task 1/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    static String booksInfo = "";
+
+
+    public static void main(String[] args) {
+        while (true) {
+            String name = inputBookName();
+            int pageCount = inputPageCount();
+            addBook(name);
+            addBook(name, pageCount);
+            //todo использовать новый метод тут
+            printInfo();
+        }
+    }
+
+    public static String inputBookName() {
+        System.out.println("Введите название книги:");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static int inputPageCount() {
+        System.out.println("Введите кол-во страниц:");
+        return new Scanner(System.in).nextInt();
+    }
+
+    public static void addBook(String bookName) {
+        addBook(bookName, 0);
+    }
+
+    public static void addBook(String bookName, int pageCount) {
+        booksInfo = booksInfo + bookName + " - " + (pageCount > 0 ? pageCount : "N/A") + " стр.\n";
+    }
+
+    public static void printInfo() {
+        System.out.println(booksInfo);
+    }
+
+}
